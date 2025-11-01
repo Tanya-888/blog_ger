f84fd87e604a745671d3c2066aa47e2fb1ce1793
Tanya-888 <vileyka.box@gmail.com>
2025-11-01 18:00:56 +0300

    git

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from flask import Flask
+from flask import render_template, url_for
+from app import create_app
 
+app = create_app()
 
+@app.route('/')
+def index():
+    return render_template('index.html')
--- model/models.py
+++
@@ -1,3 +0,0 @@
-from model.base import Base
-from model.blogitem import BlogItem
-from model.user import User
