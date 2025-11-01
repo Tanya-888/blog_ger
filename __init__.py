04bd52989f081dcb08bc97d1c199601b2f355817
Tanya-888 <vileyka.box@gmail.com>
2025-11-01 18:39:51 +0300

    git

--- __init__.py
+++ __init__.py
@@ -1,5 +1,21 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdb.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # импорт маршрутов и моделей
+    from . import models
+    from . import main
+    app.register_blueprint(main.bp)
+
     return app
